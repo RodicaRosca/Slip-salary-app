44a3b8b03933544f592bf4c44f6d607e6656fabb
Rodica Elena Rosca <Rodica-Elena.Rosca@endava.com>
2025-11-04 11:33:26 +0200

    Moved imports in logging.py

--- core/logging.py
+++ core/logging.py
@@ -1,4 +1,6 @@
 import logging
+import time
+from fastapi import Request
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
@@ -7,9 +9,6 @@ sqlalchemy_logger.setLevel(logging.ERROR)
 sqlalchemy_logger.propagate = False
 
 def setup_request_logging(app):
-    import time
-    from fastapi import Request
-
     @app.middleware("http")
     async def log_requests(request: Request, call_next):
         start_time = time.time()
